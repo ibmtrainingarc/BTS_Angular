dc5dfce48996a3864adf19c9086fc541c0f4fdc2
unknown <ibmjavanh07@iiht.tech>
2021-04-22 16:24:43 +0530

    added more validations

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  ImgPath:String= ("./assets/img/bug2.jpg");
+  ImgPath:String= ("./assets/img/bug3.jpg");
   constructor() { }
 
   ngOnInit(): void {
